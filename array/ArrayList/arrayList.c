71a4432ecf09207cc2b81d092e216b3f5cb63b35
haogroot <hsuyh82716@gmail.com>
2020-06-25 10:47:13 +0800

    Update ArrayList sample code

--- array/ArrayList/arrayList.c
+++ array/ArrayList/arrayList.c
@@ -2,95 +2,121 @@
 #include <assert.h>
 #include "arrayList.h"
 
-int init(arrayList *array, int size)
-{
-    array->data = calloc(size, sizeof(int));
-    array->size = size;
-    array->used_size = 0;
-    return 0;
-}
-
 int size(arrayList *array)
 {
     return array->size;
 }
 
-int add(arrayList *array, int element)
+bool init(arrayList *array, int size)
+{
+    array->data = (int *) calloc (size, sizeof(int));
+
+    if (array->data != NULL)
+    {
+        array->size = size;
+        array->used_size = 0;
+        return true;
+    }
+    return false;
+}
+
+/* Add element to the end of arrayList. If the capacity is not enough,
+ * add 10 more capacity. */ 
+bool add(arrayList *array, int element)
 {
     if (array->used_size == array->size)
     {
-        ensureCapacity(array, (array->size + 10));
-    } 
+        if(!ensureCapacity (array, (array->size+10)))
+            return false;
+    }
     array->data[array->used_size] = element;
     array->used_size++;
-    return 0;
+    return true;
 }
 
-int addIndex(arrayList *array, int index, int element)
+/* Add element to the index of arrayList. */
+bool addIndex(arrayList *array, size_t index, int element)
 {
-    if(index < 0 || index > array->used_size)
-    {
-        printf("index is out of range\n");
-        return -1;
-    }
-    /* Swap the array[index] and array[used_size] */
-    array->data[array->used_size] = array->data[index];
+    if (index < 0 || index > (array->used_size-1))
+        return false;
+    int tmp = array->data[index];
     array->data[index] = element;
-    return 0;
+    array->data[array->used_size] = tmp;
+    array->used_size++;
+    return true; 
 }
 
-int delIndex(arrayList *array, int index)
+/* Delete element to the index of arrayList.*/
+bool delIndex(arrayList *array, size_t index)
 {
-    if(index < 0 || index > array->used_size)
+    if (index < 0 || index > (array->used_size-1))
+        return false;
+    /* If the src and dest object in memcpy are overlapping, the behavior may be undefined. */
+    //memcpy(&array->data[index], &array->data[index+1], array->used_size-(index+1));
+    for (size_t i = index; i < (array->used_size-1); i++)
     {
-        printf("index is out of range\n");
-        return -1;
+        array->data[i] = array->data[i+1];
     }
-    memcpy(&array->data[index], &array->data[index+1], array->used_size - index -1);
     array->used_size--;
-    return 0;
+    return true;
 }
 
 void printAll(arrayList *array)
 {
-    printf ("size of ArrayList = %ld, used_size = %ld\n", array->size, array->used_size);
-    for(int i = 0; i < array->used_size; i++)
+    for (size_t i=0; i<array->used_size; i++)
     {
         printf("%d ", array->data[i]);
     }
-    printf("\n");
+    printf ("\n");
 }
 
-void ensureCapacity(arrayList *array, int size)
+/* If the argument size is larger than the size of arrayList,
+ * enlarge its size to the argument size. */
+bool ensureCapacity(arrayList *array, size_t size)
 {
     if (size > array->size)
     {
-        int *temp_buffer = calloc(size, sizeof(int));
-        memcpy(temp_buffer, array->data, (array->used_size)*sizeof(int));
-        free(array->data);
-        array->data = temp_buffer;
-        array->size = size;
+        int *tempBuffer = (int *) calloc (size, sizeof(int));
+        if (tempBuffer != NULL)
+        {
+            memcpy (tempBuffer, array->data, (array->used_size)*sizeof(int));
+            free (array->data);
+            array->data = tempBuffer;
+            array->size = size;
+            return true;
+        }
     }
+    return false;
 }
 
 int main()
 {
     arrayList list;
-    init(&list, 10);
+    assert(init(&list, 10) == true);
     assert(size(&list) == 10);
+
     for(int i=0; i<12; i++)
     {
         add(&list, i);
     }
     printAll(&list);
     assert(size(&list) == 20);
+    assert(list.used_size == 12);
+
     ensureCapacity(&list, 30);
     assert(list.size == 30);
+    
     addIndex(&list, 2, 5566);
+    printAll(&list);
     assert(list.data[2] == 5566);
-    printAll(&list);
+    assert(list.data[12] == 2);
+    assert(list.used_size == 13);
+
     delIndex(&list, 2);
-    assert(list.data[2] != 5566);
     printAll(&list);
+    assert(list.data[2] != 5566);
+    assert(list.data[2] == 3);
+    assert(list.used_size == 12);
+    assert(list.data[list.used_size-1] == 2);
     return 0;
 }
--- array/ArrayList/arrayList.h
+++ array/ArrayList/arrayList.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct arrayList
 {
@@ -11,24 +12,37 @@ typedef struct arrayList
 /* Return the capacity size of arrayList. */
 int size(arrayList *array);
 
-/* Initialize arrayList. */
-int init(arrayList *array, int size);
+/**
+ * Initialize arrayList.
+ * \return true if initialize successful, 
+ *         false if array is not NULL.
+ */
+bool init(arrayList *array, int size);
 
-/* Add element to the end of arrayList. If the capacity is not enough,
- * add 10 more capacity. */ 
-int add(arrayList *array, int element);
+/**
+ * Add element to the end of arrayList.
+ * If the capacity is not enough, add 10 more capacity.
+ */ 
+bool add(arrayList *array, int element);
 
-/* Add element to the index of arrayList. If the index is out of range,
- * return -1. */
-int addIndex(arrayList *array, int index, int element);
+/**
+ * Add element to the index of arrayList and the original element
+ * shall be move to the end of array.
+ * \return false if index is larger than the used_size-1.
+ */
+bool addIndex(arrayList *array, size_t index, int element);
 
-/* Delete element to the index of arrayList. If the index is out of range,
- * return -1. */
-int delIndex(arrayList *array, int index);
+/** 
+ * Delete element at the index of arrayList. 
+ * \return false if index is larger than the used_size -1
+ */
+bool delIndex(arrayList *array, size_t index);
 
 void printAll(arrayList *array);
 
-/* If the argument size is larger than the size of arrayList,
- * enlarge its size to the argument size. */
-void ensureCapacity(arrayList *array, int size);
+/**
+ * If the argument size is larger than the size of arrayList,
+ * enlarge its size to the argument size. 
+ */
+bool ensureCapacity(arrayList *array, size_t size);
 
